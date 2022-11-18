560f2128a4806b6f7c03647b764bd11b1af658e9
Anil Arya <87536084+anilarya-spacer@users.noreply.github.com>
2022-11-18 14:35:51 +0530

    run prettier

--- scripts/liv-modify.js
+++ scripts/liv-modify.js
@@ -1,84 +1,81 @@
- 
-import replace from 'replace-in-file'
+import replace from 'replace-in-file';
 
-let componentList  = [
-    "SlAlert",
-    "SlAnimatedImage",
-    "SlAnimation",
-    "SlAvatar",
-    "SlBadge",
-    "SlBreadcrumb",
-    "SlBreadcrumbItem",
-    "SlButton",
-    "SlButtonGroup",
-    "SlCard",
-    "SlCheckbox",
-    "SlColorPicker",
-    "SlDetails",
-    "SlDialog",
-    "SlDivider",
-    "SlDrawer",
-    "SlDropdown",
-    "SlFormatBytes",
-    "SlFormatDate",
-    "SlFormatNumber",
-    "SlIcon",
-    "SlIconButton",
-    "SlImageComparer",
-    "SlInclude",
-    "SlInput",
-    "SlMenu",
-    "SlMenuItem",
-    "SlMenuLabel",
-    "SlMutationObserver",
-    "SlPopup",
-    "SlProgressBar",
-    "SlProgressRing",
-    "SlQrCode",
-    "SlRadio",
-    "SlRadioButton",
-    "SlRadioGroup",
-    "SlRange",
-    "SlRating",
-    "SlRelativeTime",
-    "SlResizeObserver",
-    "SlSelect",
-    "SlSkeleton",
-    "SlSpinner",
-    "SlSplitPanel",
-    "SlSwitch",
-    "SlTab",
-    "SlTabGroup",
-    "SlTabPanel",
-    "SlTag",
-    "SlTextarea",
-    "SlTooltip",
-    "SlTree",
-    "SlTreeItem",
-    "SlVisuallyHidden"
-]
+let componentList = [
+  'SlAlert',
+  'SlAnimatedImage',
+  'SlAnimation',
+  'SlAvatar',
+  'SlBadge',
+  'SlBreadcrumb',
+  'SlBreadcrumbItem',
+  'SlButton',
+  'SlButtonGroup',
+  'SlCard',
+  'SlCheckbox',
+  'SlColorPicker',
+  'SlDetails',
+  'SlDialog',
+  'SlDivider',
+  'SlDrawer',
+  'SlDropdown',
+  'SlFormatBytes',
+  'SlFormatDate',
+  'SlFormatNumber',
+  'SlIcon',
+  'SlIconButton',
+  'SlImageComparer',
+  'SlInclude',
+  'SlInput',
+  'SlMenu',
+  'SlMenuItem',
+  'SlMenuLabel',
+  'SlMutationObserver',
+  'SlPopup',
+  'SlProgressBar',
+  'SlProgressRing',
+  'SlQrCode',
+  'SlRadio',
+  'SlRadioButton',
+  'SlRadioGroup',
+  'SlRange',
+  'SlRating',
+  'SlRelativeTime',
+  'SlResizeObserver',
+  'SlSelect',
+  'SlSkeleton',
+  'SlSpinner',
+  'SlSplitPanel',
+  'SlSwitch',
+  'SlTab',
+  'SlTabGroup',
+  'SlTabPanel',
+  'SlTag',
+  'SlTextarea',
+  'SlTooltip',
+  'SlTree',
+  'SlTreeItem',
+  'SlVisuallyHidden'
+];
 
-let modifiedFromList = JSON.parse(JSON.stringify(componentList)).map(comp =>{
-    return new RegExp( comp + ',', 'g');
-})
+let modifiedFromList = JSON.parse(JSON.stringify(componentList)).map(comp => {
+  return new RegExp(comp + ',', 'g');
+});
 
-let modifiedToList =JSON.parse(JSON.stringify(componentList)).map(comp =>{
-    let livcomp = `${comp}`.replace("Sl", "Liv")
-    return `${comp} as ${livcomp},`
-})
+let modifiedToList = JSON.parse(JSON.stringify(componentList)).map(comp => {
+  let livcomp = `${comp}`.replace('Sl', 'Liv');
+  return `${comp} as ${livcomp},`;
+});
 
-
-console.log(modifiedFromList, modifiedToList)
+console.log(modifiedFromList, modifiedToList);
 const options = {
   files: './dist/shoelace.js',
   from: modifiedFromList,
-  to: modifiedToList,
+  to: modifiedToList
 };
 
 try {
-    const results = await replace(options)
-    console.log('Replacement results:', results);
-  }
-  catch (error) {
-    console.error('Error occurred:', error);
-  }
+  const results = await replace(options);
+  console.log('Replacement results:', results);
+} catch (error) {
+  console.error('Error occurred:', error);
+}
